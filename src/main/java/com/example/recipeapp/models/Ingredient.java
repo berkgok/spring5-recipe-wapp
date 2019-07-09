aeea22905fec52f547e8d500f40194073d0973f8
berkgok <berkgokwork@gmail.com>
2019-07-09 11:10:18 +0300

    New Entities added One to One relation established between ingredient and UnitOfMeasure

--- src/main/java/com/example/recipeapp/models/Ingredient.java
+++ src/main/java/com/example/recipeapp/models/Ingredient.java
@@ -11,8 +11,9 @@ public class Ingredient {
     private Long id;
     private String description;
     private BigDecimal amount;
-    //todo add
-    //private UnitOfMeasure uom;
+
+    @OneToOne(fetch = FetchType.EAGER) // eager is default for one to one but just in case
+    private UnitOfMeasure uom;
 
     @ManyToOne
     private Recipe recipe;
@@ -39,6 +40,14 @@ public class Ingredient {
 
     public void setAmount(BigDecimal amount) {
         this.amount = amount;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
     }
 
     public Recipe getRecipe() {
