547c9a087274a7159c9e62117997321fe596ac1d
berkgok <berkgokwork@gmail.com>
2019-07-11 12:49:43 +0300

    New Test methods added

--- src/test/java/com/example/recipeapp/RecipeAppApplicationTests.java
+++ src/test/java/com/example/recipeapp/RecipeAppApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class RecipeAppApplicationTests {
+public class RecipeAppApplicationTests {    // this is an integration test
 
     @Test
     public void contextLoads() {
