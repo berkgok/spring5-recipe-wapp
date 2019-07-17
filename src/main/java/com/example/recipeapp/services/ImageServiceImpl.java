07008af96180abcbe2d13734d05aab8230764508
berkgok <berkgokwork@gmail.com>
2019-07-17 16:36:10 +0300

    Image upload to the database option added

--- src/main/java/com/example/recipeapp/services/ImageServiceImpl.java
+++ src/main/java/com/example/recipeapp/services/ImageServiceImpl.java
@@ -1,13 +1,47 @@
 package com.example.recipeapp.services;
 
+import com.example.recipeapp.models.Recipe;
+import com.example.recipeapp.repositories.RecipeRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 
 @Service
 public class ImageServiceImpl implements ImageService {
+
+
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl( RecipeRepository recipeService) {
+
+        this.recipeRepository = recipeService;
+    }
+
     @Override
+    @Transactional
     public void saveImageFile(Long recipeId, MultipartFile file) {
 
+        try {
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+
+            // we will get image as byte array so creating one
+            Byte[] byteObjects = new Byte[file.getBytes().length];
+
+            int i = 0;
+
+            for (byte b : file.getBytes()){
+                byteObjects[i++] = b;
+            }
+
+            recipe.setImage(byteObjects);
+
+            recipeRepository.save(recipe);
+        } catch (IOException e) {
+            //todo handle better
+            e.printStackTrace();
+        }
     }
 }
